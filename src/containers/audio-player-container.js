b41f9afdd7eb04a570ce72ba5e7174cd4e2d7ee0
Armand Adroher <aadroher@gmail.com>
2019-08-20 02:04:54 +0100

    Splits reducers and moves derived values to containers.

--- src/containers/audio-player-container.js
+++ src/containers/audio-player-container.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import AudioPlayer from '../components/audio-player';
 
-const mapStateToProps = ({ selectedSong: { isPlaying, url } }) => ({
-  src: url,
+const getSrc = ({ selectedSongId, songs = [] }) => {
+  const { url = '' } = songs.find(({ id }) => id === selectedSongId) || {};
+  return url;
+};
+
+const mapStateToProps = ({ selectedSong: { id, isPlaying }, songs }) => ({
+  src: getSrc({ selectedSongId: id, songs }),
   isPlaying,
 });
 
--- src/containers/song-list-container.js
+++ src/containers/song-list-container.js
@@ -2,7 +2,23 @@ import { connect } from 'react-redux';
 import { fetchSongs } from '../actions';
 import SongList from '../components/song-list';
 
-const mapStateToProps = ({ songs }) => ({ songs });
+const addPlayState = ({ songs, selectedSong }) => {
+  const { id: selectedSongId, isPlaying: selectedSongIsPlaying } = selectedSong;
+  return songs.map(song => {
+    const { id: songId } = song;
+    const isSelected = songId === selectedSongId;
+    const isPlaying = isSelected && selectedSongIsPlaying;
+    return {
+      ...song,
+      isSelected,
+      isPlaying,
+    };
+  });
+};
+
+const mapStateToProps = ({ songs, selectedSong }) => ({
+  songs: addPlayState({ songs, selectedSong }),
+});
 
 const mapDispatchToProps = dispatch => ({
   loadSongs: () => {
