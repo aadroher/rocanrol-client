7745289a6013e1a5256459cae6ae32478cd2fa49
Armand Adroher <aadroher@gmail.com>
2019-08-20 19:14:25 +0100

    Fixes error.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
@@ -16,17 +16,37 @@ import theme from './styles/theme';
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
+class DebugRouter extends Router {
+  constructor(props) {
+    super(props);
+    console.log('initial history is: ', JSON.stringify(this.history, null, 2));
+    this.history.listen((location, action) => {
+      console.log(
+        `The current URL is ${location.pathname}${location.search}${location.hash}`
+      );
+      console.log(
+        `The last navigation action was ${action}`,
+        JSON.stringify(this.history, null, 2)
+      );
+    });
+  }
+}
+
 const App = () => (
   <>
     <CssBaseline />
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <Router>
-          <Route path="/songs" component={SongListContainer} />
-          <Route path="/song/:id" component={Song} />
-        </Router>
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <DebugRouter>
+          <Router>
+            <Switch>
+              <Route path="/songs/:id" component={Song} />
+              <Route exact path="/songs" component={SongListContainer} />
+            </Switch>
+          </Router>
+        </DebugRouter>
+      </ThemeProvider>
+    </Provider>
   </>
 );
 
