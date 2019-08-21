b3fa6c14be347fc2990181fd1905c754ca40d118
Armand Adroher <aadroher@gmail.com>
2019-08-21 21:26:26 +0100

    Removes unused code.

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
-import { RECEIVE_SONGS_OK, PLAY_SONG, PAUSE_SONG, END_SONG } from '../actions';
-
 import selectedSong from './selected-song';
 import numPages from './num-pages';
 import currentPageNumber from './current-page-number';
