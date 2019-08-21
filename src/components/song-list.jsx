95026109f3746e094aabf1cac3148977b46f5770
Armand Adroher <aadroher@gmail.com>
2019-08-21 23:47:49 +0100

    Fixes some linting problems.

--- src/components/song-list.jsx
+++ src/components/song-list.jsx
@@ -1,23 +1,7 @@
-import React, { useEffect, useState } from 'react';
-
-import AppBar from '@material-ui/core/AppBar';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Container from '@material-ui/core/Container';
+import React, { useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
-
 import SongCardContainer from '../containers/song-card-container';
-import AudioPlayerContainer from '../containers/audio-player-container';
 import PaginationContainer from '../containers/pagination-container';
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    'padding-top': '100px',
-  },
-}));
 
 const SongList = ({
   currentPageNumber,
@@ -28,8 +12,7 @@ const SongList = ({
 }) => {
   useEffect(() => {
     loadSongs();
-  }, [url]);
-  const { container } = useStyles();
+  }, [loadSongs, url]);
   return (
     <Grid container direction="column" spacing={2}>
       {songs.map(({ id, title, author, isSelected, isPlaying }) => (
